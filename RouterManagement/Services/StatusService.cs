d59ba1ed7420bd77cbd83f9f85c554b95ed31b74
jamesgmc <jamesgm@gmail.com>
2026-03-13 05:43:04 +1000

    Error handling show retry button (#2)
    * Show retry button
    
    * show version number
    
    ---------
    
    Co-authored-by: JamesGMCoder <JamesGMCoder@users.noreply.github.com>

--- RouterManagement/Services/StatusService.cs
+++ RouterManagement/Services/StatusService.cs
@@ -5,6 +5,7 @@ namespace RouterManagement.Services
     public class StatusService
     {
         public event Action<string, string> OnStatusMessageChanged;
+        public bool IsServiceDown { get; set; } = false;
 
         public void UpdateStatusMessage(string message, string statusColor = "bg-blue-500")
         {
